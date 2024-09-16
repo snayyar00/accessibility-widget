e029f82f4033a2525479baa03a13b4890fdbcf7d
Muhammad Musa Zulfiqar <musazulfqar4@gmail.com>
2024-09-17 02:26:06 +0500

    mongo comment and google auth removed

--- app/src/containers/Auth/SocialAuth.tsx
+++ app/src/containers/Auth/SocialAuth.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 // Icons
 import FaceBookSvg from '@/assets/images/svg/facebook.svg';
-import GoogleSvg from '@/assets/images/svg/google.svg';
+// import GoogleSvg from '@/assets/images/svg/google.svg';
 import GithubSvg from '@/assets/images/svg/github.svg';
 
 const SocialAuth: React.FC = () => {
   const facebookLoginUrl = `https://www.facebook.com/v9.0/dialog/oauth?client_id=${process.env.REACT_APP_FACEBOOK_CLIENT_KEY}&redirect_uri=${process.env.REACT_APP_DOMAIN}/social/facebook/callback&scope=email`;
-  const googleLoginUrl = `https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile&response_type=code&redirect_uri=${process.env.REACT_APP_DOMAIN}/social/google/callback&client_id=${process.env.REACT_APP_GOOGLE_CLIENT_KEY}`;
+  // const googleLoginUrl = `https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile&response_type=code&redirect_uri=${process.env.REACT_APP_DOMAIN}/social/google/callback&client_id=${process.env.REACT_APP_GOOGLE_CLIENT_KEY}`;
   const githubUrl = `https://github.com/login/oauth/authorize?client_id=${process.env.REACT_APP_GITHUB_CLIENT_KEY}&redirect_uri=${process.env.REACT_APP_DOMAIN}/social/github/callback&scope=user`;
 
   return (
@@ -17,11 +17,11 @@ const SocialAuth: React.FC = () => {
           <img src={FaceBookSvg} alt="Facebook icon" />
         </a>
       )}
-      {process.env.REACT_APP_GOOGLE_CLIENT_KEY && (
+      {/* {process.env.REACT_APP_GOOGLE_CLIENT_KEY && (
         <a href={googleLoginUrl} className="border border-solid border-[#9CA3AF] border-opacity-[0.38] rounded-[5px] h-10 flex justify-center items-center">
           <img src={GoogleSvg} alt="Google icon" />
         </a>
-      )}
+      )}  */}
       {process.env.REACT_APP_GITHUB_CLIENT_KEY && (
         <a href={githubUrl} className="border border-solid border-[#9CA3AF] border-opacity-[0.38] rounded-[5px] h-10 flex justify-center items-center">
           <img src={GithubSvg} alt="Github icon" />
