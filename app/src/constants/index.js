889e7c31b043f1c93d12228e3dd14348129e2add
Muhammad Musa Zulfiqar <musazulfqar4@gmail.com>
2025-03-02 01:43:11 +0500

    app sumo constants updated for prod

--- app/src/constants/index.js
+++ app/src/constants/index.js
@@ -2,6 +2,7 @@ export const JWT_STORAGE_KEY = 'saasgear';
 
 export const APP_SUMO_BUNDLE_NAME='app sumo bundle'
 // Test mode bundle name = app sumo bundle test
-// Test mode tier plan name = app sumo tier small
 
-export const APP_SUMO_BUNDLE_NAMES=['app sumo tier small','app sumo tier medium','app sumo tier large']
+// export const APP_SUMO_BUNDLE_NAMES=['app sumo tier small','app sumo tier medium','app sumo tier large'] test mode
+
+export const APP_SUMO_BUNDLE_NAMES=['app sumo small','app sumo medium','app sumo large'] // prod
