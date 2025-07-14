c09d6dc494e885ba239f2b3d1cf8e428d84a76c8
Denis <devdenmak@gmail.com>
2025-07-14 20:55:27 +0200

    Up Rate Limit

--- api/graphql/schemas/accessibilityReport.schema.ts
+++ api/graphql/schemas/accessibilityReport.schema.ts
@@ -198,7 +198,7 @@ type Issue {
 
   extend type Query {
     getAccessibilityReport(url: String!): Report @rateLimit(limit: 3, duration: 60, message: "Too many requests. Please try again in a minute.")
-    fetchAccessibilityReportFromR2(url: String! created_at: String updated_at: String): [AccessibilityReportTableRow!]! @rateLimit(limit: 30, duration: 60, message: "Too many R2 report requests. Please try again later.")
+    fetchAccessibilityReportFromR2(url: String! created_at: String updated_at: String): [AccessibilityReportTableRow!]! @rateLimit(limit: 60, duration: 60, message: "Too many R2 report requests. Please try again later.")
     fetchReportByR2Key(r2_key: String!): Report @rateLimit(limit: 20, duration: 60, message: "Too many R2 key report requests. Please try again later.")
   }
 
