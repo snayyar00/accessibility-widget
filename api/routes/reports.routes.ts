bee3b5213e1c24b30395e226593e6d88a607e0a9
Talha899 <115891822+Talha899@users.noreply.github.com>
2025-10-15 15:51:41 +0500

    checkmark added in Issues tab

--- api/routes/reports.routes.ts
+++ api/routes/reports.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express'
 
-import { getProblemReports } from '../controllers/reports.controller'
+import { getProblemReports, toggleProblemReportFixed } from '../controllers/reports.controller'
 import { allowedOrganization, isAuthenticated } from '../middlewares/auth.middleware'
 import { moderateLimiter } from '../middlewares/limiters.middleware'
 
 const router = Router()
 
 router.post('/get-problem-reports', moderateLimiter, allowedOrganization, isAuthenticated, getProblemReports)
+router.put('/toggle-problem-report-fixed/:id', moderateLimiter, allowedOrganization, isAuthenticated, toggleProblemReportFixed)
 
 export default router
