105ba56af2490825d3e4172d782f38358d1472e4
Muhammad Musa Zulfiqar <musazulfqar4@gmail.com>
2025-10-17 00:32:56 +0500

    Update check-customer.controller.ts

--- api/controllers/stripe/check-customer.controller.ts
+++ api/controllers/stripe/check-customer.controller.ts
@@ -65,6 +65,11 @@ export async function checkCustomer(req: Request, res: Response) {
           price = await stripe.prices.retrieve(price_id.id, {
             expand: ['tiers'],
           })
+        }else if (trial_subs.data.length > 0) {
+          price_id = (trial_subs.data[0] as Stripe.Subscription).items.data[0].price
+          price = await stripe.prices.retrieve(price_id.id, {
+            expand: ['tiers'],
+          })
         }
 
         // Handle trial subscriptions
@@ -75,7 +80,7 @@ export async function checkCustomer(req: Request, res: Response) {
           const daysRemaining = Math.ceil((trialEndTimestamp! - currentTimestamp) / (60 * 60 * 24))
 
           // eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain
-          if (price?.tiers?.length! > 0) {
+          if (!price || price?.tiers?.length! > 0) {
             return res.status(200).json({
               tierPlan: true,
               isCustomer: true,
@@ -107,7 +112,7 @@ export async function checkCustomer(req: Request, res: Response) {
           const prod = await stripe.products.retrieve(String(subscriptions.data[0]?.plan?.product))
 
           // eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain
-          if (price?.tiers?.length! > 0) {
+          if (!price || price?.tiers?.length! > 0) {
             return res.status(200).json({
               tierPlan: true,
               isCustomer: true,
