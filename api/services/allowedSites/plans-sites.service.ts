cbe5a650e3d9aca5d830763212f2c6e426984f6b
Talha899 <115891822+Talha899@users.noreply.github.com>
2025-11-21 20:13:27 +0500

    fix rev v1.1

--- api/services/allowedSites/plans-sites.service.ts
+++ api/services/allowedSites/plans-sites.service.ts
@@ -128,7 +128,6 @@ export async function createSitesPlan(userId: number, paymentMethodToken: string
         subcription_id,
         is_trial: paymentMethodToken === 'Trial' ? 1 : 0,
         expired_at: expiry,
-        is_active: 1, // Explicitly set is_active to 1 so the plan is immediately queryable
       }
 
       await insertSitePlan(dataUserPlan as any)
